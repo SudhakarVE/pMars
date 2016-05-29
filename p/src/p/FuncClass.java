1b4cba93a9642b76545e3dfd17dbda5e199d9672
wel come <wel come@WELCOME>
2016-05-30 04:09:12 +0530

    updated

---
+++ p/src/p/FuncClass.java
@@ -0,0 +1,10 @@
+package p;
+
+public class FuncClass {
+public static void fDispArray(){
+	String[] sData = {"A","B","Cde"};
+	System.out.println(sData[0]);
+	System.out.println(sData[1]);
+	System.out.println(sData[2]);
+}
+}
